da2393934b9d98d819ecb1519447e112de60b514
Felipe <felipefcor@gmail.com>
2018-11-19 15:50:38 +0100

    Render final

---
+++ DomRender.js
@@ -0,0 +1,8 @@
+  ReactDOM.render(
+    <TodoList todos={['red','blue']} />,    
+    document.getElementById('container')
+    
+/*
+El principio de la historia. 
+Renderizar el elemento TodoList en el navegador con un array inicial
+*/
